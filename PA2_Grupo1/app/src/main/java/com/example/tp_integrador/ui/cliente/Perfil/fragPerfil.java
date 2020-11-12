e4d7421a83bc73e959cf35e1db059818d4b7bfb1
SebastianFont <66184845+SebastianFont@users.noreply.github.com>
2020-11-12 13:33:05 -0300

    Add files via upload
    av

--- PA2_Grupo1/app/src/main/java/com/example/tp_integrador/ui/cliente/Perfil/fragPerfil.java
+++ PA2_Grupo1/app/src/main/java/com/example/tp_integrador/ui/cliente/Perfil/fragPerfil.java
@@ -2,12 +2,15 @@ package com.example.tp_integrador.ui.cliente.Perfil;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.text.InputType;
+import android.util.Patterns;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,18 +22,20 @@ import android.widget.Toast;
 import com.example.tp_integrador.R;
 import com.example.tp_integrador.dao.UsuarioDao;
 import com.example.tp_integrador.entidad.clases.Usuario;
+import com.example.tp_integrador.ui.actLogin;
 import com.example.tp_integrador.ui.admin.navAdmin;
 import com.example.tp_integrador.ui.cliente.navCliente;
 
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
 
 
 public class fragPerfil extends Fragment {
 
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-    private Button btnContrasenia, btnDatos, btnModificar, btnVolver,btnModificarKey;
-    private EditText txtkey,txtConfirmKey;
+    private Button btnContrasenia, btnDatos, btnVolver,btnModificarKey,btnModificarDatos;
+    private EditText txtkey,txtConfirmKey,etEmail,etApellido,etNombre;
     private String username,key;
 
     @Override
@@ -73,7 +78,13 @@ public class fragPerfil extends Fragment {
         btnDatos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                crearPopUpDatos();
+                try {
+                    crearPopUpDatos();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -130,23 +141,77 @@ public class fragPerfil extends Fragment {
         });
     }
 
-    public void crearPopUpDatos(){
+    public void crearPopUpDatos() throws ExecutionException, InterruptedException {
 
         dialogBuilder = new AlertDialog.Builder(getContext());
         final View PopUp = getLayoutInflater().inflate(R.layout.popup_datos,null);
+        //Recupero los EditText
+        etEmail= PopUp.findViewById(R.id.txtEmail);
+        etNombre= PopUp.findViewById(R.id.txtNombre);
+        etApellido= PopUp.findViewById(R.id.txtApellido);
 
-        btnModificar = (Button) PopUp.findViewById(R.id.btnVolver);
+        btnVolver = (Button) PopUp.findViewById(R.id.btnVolver);
+        btnModificarDatos = (Button) PopUp.findViewById(R.id.btnConfirmar);
 
         dialogBuilder.setView(PopUp);
         dialog = dialogBuilder.create();
         dialog.show();
 
-        btnModificar.setOnClickListener(new View.OnClickListener(){
+        cargarDatos(PopUp);
+
+        btnModificarDatos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                try {
+                    ModificarDatos(view);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnVolver.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 dialog.dismiss();
             }
         });
+    }
+
+    private void cargarDatos(View v) throws ExecutionException, InterruptedException {
+        Usuario User=new Usuario();
+        User.setNameUser(username);
+        UsuarioDao x = new UsuarioDao(v.getContext(),User,3,(navCliente)getActivity());
+        String[] z = x.execute().get().split(";");
+        etNombre.setText(z[0]);
+        etApellido.setText(z[1]);
+        etEmail.setText(z[2]);
+    }
+
+    private void ModificarDatos(View v)throws ExecutionException, InterruptedException {
+      if(!(etNombre.getText().toString().isEmpty() || etApellido.getText().toString().isEmpty() ||
+              etEmail.getText().toString().isEmpty())){
+        if(validarEmail(etEmail.getText().toString())) {
+            Usuario user=new Usuario();
+            user.setNameUser(username);
+            user.setNombre(etNombre.getText().toString());
+            user.setApellido(etApellido.getText().toString());
+            user.setEmail(etEmail.getText().toString());
+            UsuarioDao UserDao = new UsuarioDao(v.getContext(),user,6, (navCliente) getActivity());
+            if(UserDao.execute().get().equals("Datos modificados exitosamente.")){
+                Toast.makeText(v.getContext(),"Datos modificados exitosamente.",Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+            else {
+                Toast.makeText(v.getContext(),"Modifique los datos.",Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(getContext(),"Email invalido",Toast.LENGTH_LONG).show();
+            etEmail.setError("verifique el formato de email ingresado");
+        }
+      }else Toast.makeText(getContext(),"Debe completar todos los campos.",Toast.LENGTH_LONG).show();
     }
 
     public void ShowPasswordMod(View v) {
@@ -171,10 +236,15 @@ public class fragPerfil extends Fragment {
             if (validarKey(txtkey.getText().toString(), txtConfirmKey.getText().toString())) {
                 Usuario user=new Usuario();
                 user.setNameUser(username);
-                user.setKeyUser(key);
+                user.setKeyUser(txtConfirmKey.getText().toString());
                 UsuarioDao UserDao = new UsuarioDao(v.getContext(),user,5, (navCliente) getActivity());
-                    if(UserDao.execute().get().equals("Usuario modificado exitosamente.")) limpiar();
-                    else Toast.makeText(v.getContext(),"Error al intentar modificar la contraseña. ",Toast.LENGTH_LONG).show();
+                if(UserDao.execute().get().equals("Contrasena modificada exitosamente.")){
+                    Toast.makeText(v.getContext(),"Contraseña modificada exitosamente.",Toast.LENGTH_LONG).show();
+                    RedirecLogin(v);
+                }
+                else {
+                    Toast.makeText(v.getContext(),"Modifique la contraseña.",Toast.LENGTH_LONG).show();
+                }
             } else {
                 Toast.makeText(v.getContext(), "Las claves no coinciden.", Toast.LENGTH_LONG).show();
                 txtkey.setError("Las claves no coinciden.");
@@ -183,22 +253,15 @@ public class fragPerfil extends Fragment {
         }else Toast.makeText(v.getContext(),"Debe completar todos los campos.",Toast.LENGTH_LONG).show();
     }
 
-    public void setearDatos(String[] datos,Usuario user) {
-        if(datos.length > 1) {
-            user.setNombre(datos[0]);
-            user.setApellido(datos[1]);
-            user.setEmail(datos[2]);
-            user.setKeyUser(txtConfirmKey.getText().toString());
-            user.setTipo_Cuenta(Integer.valueOf(datos[4]));
-            user.setEstado(true);
-        }else {
-            limpiar();
-        }
+    public void RedirecLogin(View v){
+        this.getActivity().finish();
+        Intent Reg=new Intent(this.getContext(), actLogin.class);
+        startActivity(Reg);
     }
 
-    private void limpiar() {
-        txtkey.setText(key);
-        txtConfirmKey.setText("");
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
     }
 
 }
